400410fcd436d5e4310bfa779f0309c5fae5b2c2
Dan Goodliffe <dan@randomdan.homeip.net>
2021-01-16 18:09:15 +0000

    Initial commit
    Stripped back and formatted from https://github.com/BennyQBD/ModernOpenGLTutorial/

---
+++ texture.h
@@ -0,0 +1,26 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <GL/glew.h>
+#include <string>
+
+class Texture {
+public:
+	Texture(const std::string & fileName);
+
+	void Bind();
+
+	virtual ~Texture();
+
+protected:
+private:
+	Texture(const Texture & texture) { }
+	void
+	operator=(const Texture & texture)
+	{
+	}
+
+	GLuint m_texture;
+};
+
+#endif
