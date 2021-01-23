6c417830105d27d151284546c6482f0f6eccef26
Dan Goodliffe <dan@randomdan.homeip.net>
2021-01-23 14:01:01 +0000

    Add a basic cache template

---
+++ lib/cache.h
@@ -0,0 +1,29 @@
+#ifndef CACHE_H
+#define CACHE_H
+
+#include <map>
+#include <memory>
+
+template<typename Obj> class Cache {
+public:
+	using Ptr = std::shared_ptr<Obj>;
+	[[nodiscard]] Ptr
+	get(const std::string & key)
+	{
+		if (auto e = cached.find(key); e != cached.end()) {
+			return e->second;
+		}
+		return cached.emplace(key, construct(key)).first->second;
+	}
+
+	[[nodiscard]] virtual Ptr
+	construct(const std::string & key) const
+	{
+		return std::make_shared<Obj>(key);
+	}
+
+private:
+	std::map<std::string, Ptr> cached;
+};
+
+#endif
