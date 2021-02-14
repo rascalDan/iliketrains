ec5d61a7d62e31359275f1df293c18a54e33c399
Dan Goodliffe <dan@randomdan.homeip.net>
2021-02-14 13:08:02 +0000

    Move stream operators into ns std, add support for Arc

--- utility/stream_support.hpp
+++ utility/stream_support.hpp
@@ -1,30 +1,40 @@
 #ifndef STREAM_SUPPORT_H
 #define STREAM_SUPPORT_H
 
+#include <glm/glm.hpp>
 #include <iostream>
+#include <maths.h>
 
-template<glm::length_t L, glm::length_t R, typename T, glm::qualifier Q>
-std::ostream &
-operator<<(std::ostream & s, const glm::mat<L, R, T, Q> & m)
-{
-	for (int y = 0; y < m.length(); y++) {
-		const auto & col = m[y];
-		for (int x = 0; x < col.length(); x++) {
-			s << col[x] << ", ";
+namespace std {
+	template<glm::length_t L, glm::length_t R, typename T, glm::qualifier Q>
+	std::ostream &
+	operator<<(std::ostream & s, const glm::mat<L, R, T, Q> & m)
+	{
+		for (int y = 0; y < m.length(); y++) {
+			const auto & col = m[y];
+			for (int x = 0; x < col.length(); x++) {
+				s << col[x] << ", ";
+			}
+			s << "\n";
 		}
-		s << "\n";
+		return s;
 	}
-	return s;
-}
 
-template<glm::length_t L, typename T, glm::qualifier Q>
-std::ostream &
-operator<<(std::ostream & s, const glm::vec<L, T, Q> & v)
-{
-	for (int x = 0; x < L; x++) {
-		s << v[x] << ", ";
+	template<glm::length_t L, typename T, glm::qualifier Q>
+	std::ostream &
+	operator<<(std::ostream & s, const glm::vec<L, T, Q> & v)
+	{
+		for (int x = 0; x < L; x++) {
+			s << v[x] << ", ";
+		}
+		return s;
 	}
-	return s;
+
+	std::ostream &
+	operator<<(std::ostream & s, const Arc & arc)
+	{
+		return s << arc.first << " ↺ " << arc.second;
+	}
 }
 
 #endif
