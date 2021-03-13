8f67dd5ae9aef228b1316600d58e7b7c8880fb3a
Dan Goodliffe <dan@randomdan.homeip.net>
2021-03-13 19:07:14 +0000

    Add missing virtual destructor in Renderable

--- gfx/renderable.h
+++ gfx/renderable.h
@@ -1,10 +1,16 @@
 #ifndef RENDERABLE_H
 #define RENDERABLE_H
 
+#include <special_members.hpp>
+
 class Shader;
 
 class Renderable {
 public:
+	Renderable() = default;
+	virtual ~Renderable() = default;
+	DEFAULT_MOVE_COPY(Renderable);
+
 	virtual void render(const Shader & shader) const = 0;
 };
 
