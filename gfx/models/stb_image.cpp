91dc82dc65c15c29faa48c69681487142dc1ab35
Dan Goodliffe <dan@randomdan.homeip.net>
2021-11-07 16:41:25 +0000

    Compile stb wrapper in C++ mode

--- gfx/models/stb_image.cpp
+++ gfx/models/stb_image.cpp
@@ -1,6 +1,7 @@
 #ifndef TIDY
 #	define STB_IMAGE_IMPLEMENTATION
 #	pragma GCC diagnostic ignored "-Wsign-compare"
+#	pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
 #	ifndef __clang__
 #		pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #	endif
