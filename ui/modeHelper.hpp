5a71b6e0ea40d111b3a6ce7ec39406e4d432a3af
Dan Goodliffe <dan@randomdan.homeip.net>
2022-10-08 16:09:07 +0100

    Add toolber mode switching helper

---
+++ ui/modeHelper.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <memory>
+union SDL_Event;
+
+enum ModeSecondClick { Unset, Reset, NoAction };
+template<typename Target, ModeSecondClick msc = ModeSecondClick::Unset> class Mode {
+public:
+	Target & target;
+
+	template<typename Mode, typename... Params>
+	auto
+	toggle(Params &&... params)
+	{
+		return [params..., this](const SDL_Event &) {
+			toggleSetMode<Mode>(std::forward<Params>(params)...);
+		};
+	}
+
+private:
+	template<typename Mode, typename... Params>
+	void
+	toggleSetMode(Params &&... params)
+	{
+		if (dynamic_cast<Mode *>(target.get())) {
+			if constexpr (msc == ModeSecondClick::Unset) {
+				target.reset();
+			}
+			if constexpr (msc == ModeSecondClick::Reset) {
+				target = std::make_unique<Mode>(std::forward<Params>(params)...);
+			}
+		}
+		else {
+			target = std::make_unique<Mode>(std::forward<Params>(params)...);
+		}
+	}
+};
