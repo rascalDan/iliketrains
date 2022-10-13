b11139145aea75465f60d6cc311aa23f27a83814
Dan Goodliffe <dan@randomdan.homeip.net>
2022-10-13 20:25:09 +0100

    Inheritable of standard typedefs

---
+++ lib/stdTypeDefs.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+template<typename T> struct StdTypeDefs {
+	using Ptr = std::shared_ptr<T>;
+	using CPtr = std::shared_ptr<const T>;
+	using WPtr = std::weak_ptr<const T>;
+	using Collection = std::vector<Ptr>;
+	using CCollection = std::vector<CPtr>;
+	using WCollection = std::vector<WPtr>;
+};
