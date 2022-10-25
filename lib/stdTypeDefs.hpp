b656ad280092832ce47df3b7752be0a8052fcd20
Dan Goodliffe <dan@randomdan.homeip.net>
2022-10-25 17:50:04 +0100

    Add AnyPtr for accepting pointers of any kind into a function

--- lib/stdTypeDefs.hpp
+++ lib/stdTypeDefs.hpp
@@ -3,7 +3,34 @@
 #include <memory>
 #include <vector>
 
+template<typename T> struct AnyPtr {
+	// cppcheck-suppress noExplicitConstructor
+	AnyPtr(T * p) : ptr {p} { }
+	// cppcheck-suppress noExplicitConstructor
+	template<typename S> AnyPtr(const S & p) : ptr {p.get()} { }
+	auto
+	get() const
+	{
+		return ptr;
+	}
+	auto
+	operator->() const
+	{
+		return ptr;
+	}
+	auto &
+	operator*() const
+	{
+		return *ptr;
+	}
+
+private:
+	T * ptr;
+};
+
 template<typename T> struct StdTypeDefs {
+	using AnyPtr = ::AnyPtr<T>;
+	using AnyCPtr = ::AnyPtr<const T>;
 	using Ptr = std::shared_ptr<T>;
 	using CPtr = std::shared_ptr<const T>;
 	using WPtr = std::weak_ptr<const T>;
