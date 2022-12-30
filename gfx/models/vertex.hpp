e29634fa618cc3bbc8cee60f4f6133385ff2f9c1
Dan Goodliffe <dan@randomdan.homeip.net>
2022-12-30 14:57:39 +0000

    Make Vertex constexpr

--- gfx/models/vertex.hpp
+++ gfx/models/vertex.hpp
@@ -4,7 +4,7 @@
 
 class Vertex {
 public:
-	Vertex(glm::vec3 pos, glm::vec2 texCoord, glm::vec3 normal) :
+	constexpr Vertex(glm::vec3 pos, glm::vec2 texCoord, glm::vec3 normal) :
 		pos {std::move(pos)}, texCoord {std::move(texCoord)}, normal {std::move(normal)}
 	{
 	}
