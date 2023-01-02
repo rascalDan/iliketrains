349dd957ea05c1cd9289646db6888a993c8ee02f
Dan Goodliffe <dan@randomdan.homeip.net>
2023-01-02 17:13:01 +0000

    Add gl_traits for all the common types

--- lib/gl_traits.hpp
+++ lib/gl_traits.hpp
@@ -3,10 +3,39 @@
 #include <GL/glew.h>
 #include <array>
 #include <glm/common.hpp>
+#include <glm/fwd.hpp>
 
 template<typename T> struct gl_traits;
-template<> struct gl_traits<float> {
+template<> struct gl_traits<glm::f32> {
 	static constexpr GLenum type {GL_FLOAT};
+	static constexpr GLint size {1};
+};
+template<> struct gl_traits<glm::f64> {
+	static constexpr GLenum type {GL_DOUBLE};
+	static constexpr GLint size {1};
+};
+template<> struct gl_traits<glm::int8> {
+	static constexpr GLenum type {GL_BYTE};
+	static constexpr GLint size {1};
+};
+template<> struct gl_traits<glm::int16> {
+	static constexpr GLenum type {GL_SHORT};
+	static constexpr GLint size {1};
+};
+template<> struct gl_traits<glm::int32> {
+	static constexpr GLenum type {GL_INT};
+	static constexpr GLint size {1};
+};
+template<> struct gl_traits<glm::uint8> {
+	static constexpr GLenum type {GL_UNSIGNED_BYTE};
+	static constexpr GLint size {1};
+};
+template<> struct gl_traits<glm::uint16> {
+	static constexpr GLenum type {GL_UNSIGNED_SHORT};
+	static constexpr GLint size {1};
+};
+template<> struct gl_traits<glm::uint32> {
+	static constexpr GLenum type {GL_UNSIGNED_INT};
 	static constexpr GLint size {1};
 };
 
