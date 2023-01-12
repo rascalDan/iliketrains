1a3e85753e9dd188fc1c6539f8e8a695597c4c55
Dan Goodliffe <dan@randomdan.homeip.net>
2023-01-12 19:32:50 +0000

    Fix wrap/clamp settings on icon textures

--- ui/icon.cpp
+++ ui/icon.cpp
@@ -13,8 +13,8 @@ Icon::Icon(const Image & tex) : size {tex.width, tex.height}
 {
 	glBindTexture(GL_TEXTURE_2D, m_texture);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
