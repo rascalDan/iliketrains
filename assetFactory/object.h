6772443b0ef78ce7cbdd815103d7653f2b243e36
Dan Goodliffe <dan@randomdan.homeip.net>
2023-02-21 01:05:38 +0000

    Add missing override

--- assetFactory/object.h
+++ assetFactory/object.h
@@ -8,7 +8,7 @@ class Object : public StdTypeDefs<Object>, public Shape {
 public:
 	Object(std::string i);
 
-	CreatedFaces createMesh(ModelFactoryMesh & mesh, const Mutation::Matrix & mutation) const;
+	CreatedFaces createMesh(ModelFactoryMesh & mesh, const Mutation::Matrix & mutation) const override;
 
 	Use::Collection uses;
 	std::string id;
