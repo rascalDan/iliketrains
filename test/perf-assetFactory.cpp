648025daa969b90bd43eb8865142187015082876
Dan Goodliffe <dan@randomdan.homeip.net>
2023-03-05 02:45:21 +0000

    Unify asset factory perf tests as it's now a combined load/create operation

--- test/perf-assetFactory.cpp
+++ test/perf-assetFactory.cpp
@@ -7,26 +7,13 @@
 static void
 brush47xml_load(benchmark::State & state)
 {
+	TestMainWindow window;
+
 	for (auto _ : state) {
 		benchmark::DoNotOptimize(AssetFactory::loadXML(RESDIR "/brush47.xml"));
 	}
 }
 
-static void
-brush47xml_mesh(benchmark::State & state)
-{
-	TestMainWindow window;
-
-	const auto mf = AssetFactory::loadXML(RESDIR "/brush47.xml");
-	const auto brush47 = mf->assets.at("brush-47");
-	for (auto _ : state) {
-		std::for_each(brush47->meshes.begin(), brush47->meshes.end(), [](const FactoryMesh::CPtr & factoryMesh) {
-			factoryMesh->createMesh();
-		});
-	}
-}
-
 BENCHMARK(brush47xml_load);
-BENCHMARK(brush47xml_mesh);
 
 BENCHMARK_MAIN();
