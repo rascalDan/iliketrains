f412b04cc367168b1a6f921dfa1171a17e1353fc
Dan Goodliffe <dan@randomdan.homeip.net>
2023-03-18 02:12:21 +0000

    Use indices instead of triangulation

--- assetFactory/factoryMesh.cpp
+++ assetFactory/factoryMesh.cpp
@@ -10,24 +10,31 @@ FactoryMesh::createMesh() const
 	for (const auto & use : uses) {
 		use->createMesh(mesh, 1);
 	}
-	mesh.garbage_collection();
 
-	mesh.triangulate();
 	mesh.update_face_normals();
 	mesh.update_vertex_normals();
 	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
 	for (const auto & face : mesh.faces()) {
 		const auto smooth = mesh.property(mesh.smoothFaceProperty, face);
 		const auto colour = mesh.color(face);
-		for (const auto & vertex : mesh.fv_range(face)) {
+		auto vrange = mesh.fv_range(face);
+		const unsigned int start = static_cast<unsigned int>(vertices.size());
+		for (const auto & vertex : vrange) {
 			const auto textureUV = mesh.texcoord2D(vertex);
 			vertices.emplace_back(mesh.point(vertex), textureUV,
 					smooth ? mesh.property(mesh.vertex_normals_pph(), vertex)
 						   : mesh.property(mesh.face_normals_pph(), face),
 					colour);
 		}
+		const auto vcount = std::distance(vrange.begin(), vrange.end());
+		for (unsigned int i = 2; i < vcount; i++) {
+			indices.push_back(start);
+			indices.push_back(start + i - 1);
+			indices.push_back(start + i);
+		}
 	}
-	return std::make_shared<Mesh>(vertices, vectorOfN(vertices.size()));
+	return std::make_shared<Mesh>(vertices, indices);
 }
 
 bool
