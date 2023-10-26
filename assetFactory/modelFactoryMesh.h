6ec18905acdb9b30e5bfc5eee9be0182ce1744c9
Dan Goodliffe <dan.goodliffe@octal.co.uk>
2023-10-26 20:34:55 +0100

    Move OpenMesh/GLM compatibility structs to common place

--- assetFactory/modelFactoryMesh.h
+++ assetFactory/modelFactoryMesh.h
@@ -2,36 +2,11 @@
 
 #include "modelFactoryMesh_fwd.h"
 #include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
-#include <OpenMesh/Core/Mesh/Traits.hh>
 #include <glad/gl.h>
-#include <glm/geometric.hpp>
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
-
-namespace glm {
-	template<length_t L, typename T, qualifier Q>
-	auto
-	norm(const vec<L, T, Q> & v)
-	{
-		return length(v);
-	}
-
-	template<length_t L, typename T, qualifier Q, typename S>
-	auto
-	vectorize(vec<L, T, Q> & v, S scalar)
-	{
-		v = vec<L, T, Q> {static_cast<T>(scalar)};
-	}
-}
-
-namespace OpenMesh {
-	template<glm::length_t L, typename T, glm::qualifier Q> struct vector_traits<glm::vec<L, T, Q>> {
-		using vector_type = glm::vec<L, T, Q>;
-		using value_type = T;
-		static constexpr glm::length_t size_ = L;
-	};
-}
+#include <thirdparty/openmesh/glmcompat.h>
 
 struct ModelFactoryTraits : public OpenMesh::DefaultTraits {
 	FaceAttributes(OpenMesh::Attributes::Normal | OpenMesh::Attributes::Status | OpenMesh::Attributes::Color);
