442e226dbdfbe8f281f1f23805695ebb1a5fedc3
Dan Goodliffe <dan@randomdan.homeip.net>
2023-12-17 18:55:26 +0000

    Add 64bit integer types for calculations

--- config/types.h
+++ config/types.h
@@ -6,11 +6,13 @@
 using Distance = float; // deprecate
 using RelativeDistance = float;
 using GlobalDistance = int32_t;
+using CalcDistance = int64_t;
 using Angle = float;
 
 template<glm::length_t D> using Position = glm::vec<D, Distance>; // deprecate
 template<glm::length_t D> using RelativePosition = glm::vec<D, RelativeDistance>;
 template<glm::length_t D> using GlobalPosition = glm::vec<D, GlobalDistance>;
+template<glm::length_t D> using CalcPosition = glm::vec<D, CalcDistance>;
 template<glm::length_t D> using Size = glm::vec<D, Distance>;
 template<glm::length_t D> using Scale = glm::vec<D, float>;
 template<glm::length_t D> using Direction = glm::vec<D, float>;
@@ -27,6 +29,9 @@ using RelativePosition4D = RelativePosition<4>;
 using GlobalPosition2D = GlobalPosition<2>;
 using GlobalPosition3D = GlobalPosition<3>;
 using GlobalPosition4D = GlobalPosition<4>;
+using CalcPosition2D = CalcPosition<2>;
+using CalcPosition3D = CalcPosition<3>;
+using CalcPosition4D = CalcPosition<4>;
 using Size2D = Size<2>;
 using Size3D = Size<3>;
 using Scale2D = Scale<2>;
