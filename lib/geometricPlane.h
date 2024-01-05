7ad46a0e239c7e68c4436e632a77a0d8a14b6333
Dan Goodliffe <dan@randomdan.homeip.net>
2024-01-05 20:08:26 +0000

    Inline functions of GeometricPlane

--- lib/geometricPlane.h
+++ lib/geometricPlane.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include "config/types.h"
+#include "ray.h"
+#include <glm/geometric.hpp>
+#include <glm/gtx/intersect.hpp>
 #include <glm/vec3.hpp>
 #include <optional>
-
-class Ray;
 
 class GeometricPlane {
 public:
@@ -17,8 +18,27 @@ public:
 	Position3D origin;
 	Normal3D normal;
 
-	[[nodiscard]] PlaneRelation getRelation(Position3D point) const;
-	[[nodiscard]] std::optional<DistAndPosition> getRayIntersectPosition(const Ray &) const;
+	[[nodiscard]] inline PlaneRelation
+	getRelation(Position3D point) const
+	{
+		const auto d = glm::dot(normal, point - origin);
+		return d < 0.F ? PlaneRelation::Below : d > 0.F ? PlaneRelation::Above : PlaneRelation::On;
+	}
 
-	static bool isIntersect(PlaneRelation a, PlaneRelation b);
+	[[nodiscard]] inline std::optional<DistAndPosition>
+	getRayIntersectPosition(const Ray & ray) const
+	{
+		float dist {};
+		if (!glm::intersectRayPlane(ray.start, ray.direction, origin, normal, dist)) {
+			return {};
+		}
+		return DistAndPosition {dist, ray.start + (ray.direction * dist)};
+	}
+
+	inline static bool
+	isIntersect(PlaneRelation a, PlaneRelation b)
+	{
+		return ((a == PlaneRelation::Above && b == PlaneRelation::Below)
+				|| (a == PlaneRelation::Below && b == PlaneRelation::Above));
+	}
 };
