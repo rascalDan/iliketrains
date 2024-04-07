8231a803cbb561715ed0df5a1e393f002874b5db
Dan Goodliffe <dan@randomdan.homeip.net>
2024-04-07 18:24:32 +0100

    Create water squares/polygons only where required

--- game/water.cpp
+++ game/water.cpp
@@ -12,34 +12,80 @@
 #include <iterator>
 #include <location.h>
 #include <maths.h>
+#include <set>
 #include <utility>
 #include <vector>
+
+namespace glm {
+	bool
+	operator<(const GlobalPosition2D a, const GlobalPosition2D b)
+	{
+		return std::tie(a.x, a.y) < std::tie(b.x, b.y);
+	}
+}
 
 Water::Water(std::shared_ptr<GeoData> tm) : geoData {std::move(tm)}, water {std::make_shared<Texture>("water.png")}
 {
 	generateMeshes();
 }
 
+static constexpr GlobalDistance MIN_HEIGHT = 1'000;
+static constexpr GlobalDistance TILE_SIZE = 8'192;
+static constexpr GlobalDistance BORDER = TILE_SIZE / 2;
+
 void
 Water::generateMeshes()
 {
+	// Map out where a water square needs to exist to cover all terrain faces with a low vertex
+	std::set<GlobalPosition2D> waterPositions;
+	std::for_each(geoData->vertices_sbegin(), geoData->vertices_end(), [this, &waterPositions](const auto vh) {
+		if (geoData->point(vh).z < MIN_HEIGHT) {
+			std::for_each(geoData->vf_begin(vh), geoData->vf_end(vh),
+					[done = std::set<OpenMesh::FaceHandle>(), this, &waterPositions](const auto fh) mutable {
+						if (done.insert(fh).second) {
+							const auto getrange = [this, fh](glm::length_t axis) {
+								const auto mme = std::minmax_element(geoData->fv_begin(fh), geoData->fv_end(fh),
+										[this, axis](const auto vh1, const auto vh2) {
+											return geoData->point(vh1)[axis] < geoData->point(vh2)[axis];
+										});
+
+								return std::make_pair((geoData->point(*mme.first)[axis] - BORDER) / TILE_SIZE,
+										(geoData->point(*mme.second)[axis] + BORDER) / TILE_SIZE);
+							};
+							const auto xrange = getrange(0);
+							const auto yrange = getrange(1);
+							for (auto x = xrange.first; x < xrange.second; x++) {
+								for (auto y = yrange.first; y < yrange.second; y++) {
+									waterPositions.emplace(x, y);
+								}
+							}
+						}
+					});
+		}
+	});
+
 	std::vector<unsigned int> indices;
-	indices.reserve(geoData->n_faces() * 3);
 	std::vector<Vertex> vertices;
-	vertices.reserve(geoData->n_vertices());
-	std::map<GeoData::VertexHandle, size_t> vertexIndex;
-	std::transform(geoData->vertices_sbegin(), geoData->vertices_end(), std::back_inserter(vertices),
-			[this, &vertexIndex](const GeoData::VertexHandle v) {
-				vertexIndex.emplace(v, vertexIndex.size());
-				const auto p = geoData->point(v);
-				return Vertex {p, RelativePosition2D(p) / 10000.F, geoData->normal(v)};
-			});
-	std::for_each(
-			geoData->faces_sbegin(), geoData->faces_end(), [this, &vertexIndex, &indices](const GeoData::FaceHandle f) {
-				std::transform(geoData->fv_begin(f), geoData->fv_end(f), std::back_inserter(indices),
-						[&vertexIndex](const GeoData::VertexHandle v) {
-							return vertexIndex[v];
-						});
+	std::map<GlobalPosition2D, size_t> vertexIndex;
+	std::for_each(waterPositions.begin(), waterPositions.end(),
+			[&indices, &vertices, &vertexIndex, extents = geoData->getExtents(), this](const GlobalPosition2D p) {
+				std::array<unsigned int, 4> currentIndices {};
+				auto out = currentIndices.begin();
+				for (auto x : {0, TILE_SIZE}) {
+					for (auto y : {0, TILE_SIZE}) {
+						const auto pos = (p * TILE_SIZE) + GlobalPosition2D {x, y};
+						const auto v = vertexIndex.emplace(pos, vertices.size());
+						if (v.second) {
+							const auto cpos = glm::clamp(pos, std::get<0>(extents).xy(), std::get<1>(extents).xy());
+							vertices.emplace_back(RelativePosition3D {geoData->positionAt(cpos)},
+									TextureRelCoord(pos / TILE_SIZE), up);
+						}
+						*out++ = static_cast<unsigned int>(v.first->second);
+					}
+				}
+				for (const auto i : {0U, 3U, 1U, 0U, 2U, 3U}) {
+					indices.push_back(currentIndices[i]);
+				}
 			});
 	meshes.create<Mesh>(vertices, indices);
 }
