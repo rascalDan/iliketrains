a39e5a0bc8df00d79773a0cc3318125c736028e4
Dan Goodliffe <dan@randomdan.homeip.net>
2024-07-21 01:44:42 +0100

    Pull material detail function into a common file

---
+++ gfx/gl/shaders/materialDetail.glsl
@@ -0,0 +1,14 @@
+layout(binding = 1) uniform usampler2DRect materialData;
+
+MaterialDetail
+getMaterialDetail(uint midx)
+{
+	if (midx > 0u) {
+		const vec4 sPosSize = texture(materialData, uvec2(0, midx - 1u));
+		const uvec4 sMode = texture(materialData, uvec2(1, midx - 1u));
+		const uint mapmodeU = sMode.x & 0xFu;
+		const uint mapmodeV = (sMode.x & 0xF0u) >> 1;
+		return MaterialDetail(sPosSize.xy, sPosSize.zw, uvec2(mapmodeU, mapmodeV));
+	}
+	return MaterialDetail(vec2(0, 0), vec2(0, 0), uvec2(0, 0));
+}
