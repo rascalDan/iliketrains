940f49d1c33a9affdd73071b1ed6e263ff33b2a1
Dan Goodliffe <dan@randomdan.homeip.net>
2026-02-14 12:29:26 +0000

    Ensure ANALYSIS_DIRECTORY root folder is created up front

--- test/testHelpers.cpp
+++ test/testHelpers.cpp
@@ -2,5 +2,7 @@
 
 const std::filesystem::path ANALYSIS_DIRECTORY = []() {
 	auto xdgRuntimeDir = getenv("XDG_RUNTIME_DIR");
-	return std::filesystem::path {xdgRuntimeDir ? xdgRuntimeDir : "/tmp"} / "ilt-output";
+	auto out = std::filesystem::path {xdgRuntimeDir ? xdgRuntimeDir : "/tmp"} / "ilt-output";
+	std::filesystem::create_directories(out);
+	return out;
 }();
