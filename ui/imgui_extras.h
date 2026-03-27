b69b78ed213041a4cbc5d06fee09cbda1c57dd17
Dan Goodliffe <dan.goodliffe@octal.co.uk>
2026-03-27 09:57:29 +0000

    Add wrapper for ImGui::TextEx for any contiguous char range

--- ui/imgui_extras.h
+++ ui/imgui_extras.h
@@ -1,4 +1,7 @@
 #include <imgui.h>
+#include <imgui_internal.h>
+#include <memory>
+#include <ranges>
 
 namespace IltGui {
 	// NOLINTBEGIN(readability-identifier-naming)
@@ -6,5 +9,16 @@ namespace IltGui {
 			ImGuiWindowFlags windowFlags = 0);
 	bool BeginToolbar(const char * name, ImGuiDir dir, float axisSize, ImGuiWindowFlags windowFlags = 0);
 	void EndToolbar();
+
+	void
+	Text(std::ranges::contiguous_range auto text, ImGuiTextFlags flags = ImGuiTextFlags_None)
+		requires requires {
+			{ std::to_address(text.begin()) } -> std::convertible_to<const char *>;
+			{ std::to_address(text.end()) } -> std::convertible_to<const char *>;
+		}
+	{
+		ImGui::TextEx(std::to_address(text.begin()), std::to_address(text.end()), flags);
+	}
+
 	// NOLINTEND(readability-identifier-naming)
 }
